6c33e44cc4acc936d955c3db01f845c1f14686b6
AleksandrRyb <inkognito19931@gmail.com>
2023-09-11 09:59:40 +0300

    delete misspell

--- helper/auth/verify-token.ts
+++ helper/auth/verify-token.ts
@@ -2,7 +2,7 @@ import * as jwt from 'jsonwebtoken';
 
 export const verifyToken = (token: string): Promise<any> => {
   return new Promise((resolve, reject) => {
-    jwt.verify(token, process.env.JWT_SEKRET, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         return reject(err);
       }
