9fca557565ffd0277635c9012352905a85541aa1
AleksandrRyb <inkognito19931@gmail.com>
2023-09-13 12:38:20 +0300

    add fix in authorizer, add a token creation logic in trycatch block

--- api/users/lambdas/authorizer-func.ts
+++ api/users/lambdas/authorizer-func.ts
@@ -1,22 +1,21 @@
 import { APIGatewayAuthorizerResult } from 'aws-lambda';
 import { verifyToken } from '@helper/auth/verify-token';
 
-export const handler = async (event) => {
-  const token = event.headers.authorization.split(' ')[1];
-
+export const handler = (event, _, callback) => {
   try {
+    const token = event.headers.authorization.split(' ')[1];
     const decoded = verifyToken(token);
 
     const currentTimeInSeconds = Math.floor(Date.now() / 1000);
     if (decoded.exp && decoded.exp < currentTimeInSeconds) {
-      return generateUnauthorizedResponse('Token expired');
+      throw new Error();
     }
 
     const principalId = decoded.email;
     const policy = generatePolicy(principalId, 'Allow', '*', {});
     return policy;
   } catch (error) {
-    return generateUnauthorizedResponse('Unauthorized');
+    callback('Unauthorized');
   }
 };
 
@@ -43,10 +42,3 @@ function generatePolicy<C extends APIGatewayAuthorizerResult['context']>(
 
   return authResponse;
 }
-
-const generateUnauthorizedResponse = async (message) => {
-  return {
-    statusCode: 401,
-    body: JSON.stringify(message),
-  };
-};
